010e5206885b32107709e07dfbc3d436b6676890
Higbee-Keith <hig12012@byui.edu>
2016-05-28 14:48:40 -0700

    updated create function

--- app/src/main/java/com/byui/cs246/multithreaded/MainActivity.java
+++ app/src/main/java/com/byui/cs246/multithreaded/MainActivity.java
@@ -1,13 +1,96 @@
 package com.byui.cs246.multithreaded;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String filename = "numbers.txt";
+    File file;
+    ArrayList<String> list;
+    ArrayAdapter<String> numbersAdapter;
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the Create button is clicked **/
+    public void createFile(View view) throws IOException {
+        file = new File(getFilesDir(), filename);
+        BufferedWriter buf = new BufferedWriter(new FileWriter(file));
+
+        /* Append to the file */
+        try {
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    buf.write(i);
+                    System.out.println(i);
+                    Thread.sleep(250);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            buf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** Called when the Load button is clicked **/
+    public void loadFile(View view) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        list = new ArrayList<String>();
+
+        // Read each line and pause
+        try {
+            for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+                list.add(line);
+                Thread.sleep(250);
+            }
+            //while (bufferedReader.readLine() != null) {
+            //    list.add(bufferedReader.readLine());
+            //    Thread.sleep(250);
+            //}
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Add to List View using ArrayAdapter
+        numbersAdapter = new ArrayAdapter<String>(this,
+                R.layout.activity_main, R.id.textView, list);
+
+        // Connect ArrayAdapter to ListView to be populated
+        listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(numbersAdapter);
+    }
+
+    /** Called when the Clear button is clicked **/
+    public void clearList(View view) {
+    /*
+        1. clear the list. (Hint: Clear the ArrayAdapter)
+    */
+        list.clear();
+
     }
 }
